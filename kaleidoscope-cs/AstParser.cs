8f16eb715764e9045679e956a846af7ad9dc5955
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2020-06-08 15:21:14 +0200

    Parsing top-level elements

--- kaleidoscope-cs/AstParser.cs
+++ kaleidoscope-cs/AstParser.cs
@@ -21,8 +21,14 @@ namespace Kaleidoscope.Ast
 		// Parse a top-level element and return it.
 		public TopLevelElement Parse()
 		{
-			// TODO
-			return null;
+			switch (this._token.Type)
+			{
+				case TokenType.Keyword when (this._token.Keyword == Keyword.Dec): return ParseFunctionDeclaration();
+				case TokenType.Keyword when (this._token.Keyword == Keyword.Def): return ParseFunctionDefinition();
+				case TokenType.EndOfFile: return null;
+
+				default: throw new FormatException($"Expected function declaration / definition or end of file, but got '{ this._token }'.");
+			}
 		}
 
 		// Get the precedence of the given operator.
@@ -53,6 +59,116 @@ namespace Kaleidoscope.Ast
 			{
 				this._token = this._lexer.Lex();
 			} while (this._token.Type == TokenType.Comment);
+		}
+
+		// Parse a generic expression (= a primary expression + (op + primary expression)*).
+		private Expression ParseExpression()
+		{
+			// TODO
+			return new LiteralExpression(42);
+		}
+
+		// Parse a function prototype.
+		private FunctionPrototype ParseFunctionPrototype()
+		{
+			// Parse the function name.
+			if (this._token.Type != TokenType.Identifier)
+			{
+				throw new FormatException($"Expected identifier in function prototype, but got '{ this._token }'.");
+			}
+
+			var name = this._token.Identifier;
+			EatToken();
+
+			// Parse the opening bracket.
+			if ((this._token.Type != TokenType.Bracket) || (this._token.Bracket != Bracket.RoundStart))
+			{
+				throw new FormatException($"Expected '(' in function prototype, but got '{ this._token }'.");
+			}
+
+			EatToken();
+
+			// Start a list of parameter names.
+			var parameterNames = new List<string>();
+
+			// Check if this is the "no parameter" case as in "func()".
+			if (this._token.Type != TokenType.Bracket)
+			{
+				while (true)
+				{
+					// Parse the current parameter name.
+					if (this._token.Type != TokenType.Identifier)
+					{
+						throw new FormatException($"Expected identifier in function prototype, but got '{ this._token }'.");
+					}
+
+					parameterNames.Add(this._token.Identifier);
+					EatToken();
+
+					// If we now encounter a bracket, we are done.
+					if (this._token.Type == TokenType.Bracket)
+					{
+						break;
+					}
+
+					// Otherwise, this must be a parameter separator.
+					if (this._token.Type != TokenType.ParameterSeparator)
+					{
+						throw new FormatException($"Expected ')' or ',' in function prototype, but got '{ this._token }'.");
+					}
+
+					EatToken();
+				}
+			}
+
+			// Validate and eat the closing bracket.
+			if (this._token.Bracket != Bracket.RoundEnd)
+			{
+				throw new FormatException($"Expected ')' in function prototype, but got '{ this._token }'.");
+			}
+
+			EatToken();
+
+			return new FunctionPrototype(name, parameterNames);
+		}
+
+		//Parse a function declaration.
+		private TopLevelElement ParseFunctionDeclaration()
+		{
+			//Parse the "dec" keyword.
+			if ((this._token.Type != TokenType.Keyword) || (this._token.Keyword != Keyword.Dec))
+			{
+				throw new FormatException($"Expected 'dec' in function prototype, but got '{ this._token }'.");
+			}
+
+			EatToken();
+
+			// Parse the prototype.
+			var prototype = ParseFunctionPrototype();
+
+			// Build the final declaration.
+			return new FunctionDeclaration(prototype);
+		}
+
+		// Parse a function definition.
+		private TopLevelElement ParseFunctionDefinition()
+		{
+			//Parse the "def" keyword.
+			if ((this._token.Type != TokenType.Keyword) || (this._token.Keyword != Keyword.Def))
+			{
+				throw new FormatException($"Expected 'def' in function prototype, but got '{ this._token }'.");
+			}
+
+			EatToken();
+
+			// Parse the prototype.
+			var prototype = ParseFunctionPrototype();
+
+			// Parse the body expression.
+			var body = ParseExpression();
+
+			// Build the final definition.
+			return new FunctionDefinition(prototype, body);
 		}
 	}
 }
